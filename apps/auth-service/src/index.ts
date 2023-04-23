3828a87367d5b2a72542e17f1ee488f3aa265c34
djnovin <hello@novinnoori.com>
2023-04-24 00:16:38 +1000

    changes

--- apps/auth-service/src/index.ts
+++ apps/auth-service/src/index.ts
@@ -1,12 +1,90 @@
-import express from "express";
-
+import express, { Request, Response } from 'express';
+import passport from 'passport';
+import session from "express-session";
+import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 const PORT = process.env.PORT || 8001;
 const HOST = process.env.HOST || "localhost";
+import Redis from 'ioredis';
+import RedisStore from 'connect-redis';
 
 const app = express();
 
-app.get("/api/v1/auth", (req, res) => {
-  res.json("Hello from auth-service");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const redisClient = new Redis({
+  host: 'localhost',
+  port: 6379,
+  password: 'password',
+});
+
+app.use(session({
+  store: redisStore,
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.authenticate('session'));
+
+const Users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "admin",
+    email: ""
+  }
+];
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "secret",
+  issuer: "auth-service",
+  audience: "client",
+};
+
+type Payload = {
+  id: number;
+  user: string;
+  email: string;
+};
+
+const verify = (payload: Payload, done: any) => {
+  const user = Users.find((user) => user.id === payload.id);
+  if (user) {
+    return done(null, user);
+  } else {
+    return done(null, false);
+  }
+};
+
+passport.use(new JwtStrategy(opts, verify));
+
+const localopts = {
+  usernameField: "username",
+  passwordField: "password",
+  session: false,
+};
+ 
+const localverify = (username: string, password:string, done: any) => {
+  const user = Users.find((user) => user.username === username);
+  if (user) {
+    return done(null, user);
+  } else {
+    return done(null, false);
+  }
+};
+
+passport.use(new LocalStrategy(localopts, localverify));
+
+app.get("/api/v1/auth/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+}));
+
+app.get("/api/v1/auth/logout", (req, res) => {
+
 });
 
 app.listen(PORT, () => {
--- apps/user-service/src/index.ts
+++ apps/user-service/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import Redis from "ioredis";
 
 const PORT = process.env.PORT || 8001;
 const HOST = process.env.HOST || "localhost";
 
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+
 const app = express();
+
+const redis = new Redis();
 
 app.get("/api/v1/users", (req, res) => {
   res.json({
